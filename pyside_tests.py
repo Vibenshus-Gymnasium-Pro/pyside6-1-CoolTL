71a33932a9e96a20025bd47c9c5067b6174a8f54
CoolTL <thomasliisborg@gmail.com>
2025-08-26 15:00:23 +0200

    added most of the maths
    the code doesnt work because of the pushButton, but i will look at that later

--- pyside_tests.py
+++ pyside_tests.py
@@ -2,51 +2,57 @@ import sys
 
 from PySide6.QtWidgets import (
     QApplication,
-    QCheckBox,
-    QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
-    QDial,
-    QDoubleSpinBox,
-    QFontComboBox,
-    QLabel,
-    QLCDNumber,
-    QLineEdit,
     QMainWindow,
-    QProgressBar,
+    QLabel,
     QPushButton,
-    QRadioButton,
-    QSlider,
-    QSpinBox,
-    QTimeEdit,
-    QVBoxLayout,
-    QWidget,
+    QDoubleSpinBox,
 )
-from PySide6.QtUiTools import QUiLoader
 from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtUiTools import QUiLoader
 
-def reelle_loesninger_til_andengradsligning(a, b, c):
+
+def zero_check(a, b, c):
+    """Funktionen checker om a er lig 0"""
+    if float(a) == 0.0:
+        window.solution_checker.setText("a kan ikke være 0")
+        return False
+    else:
+        return True
+
+def maths(a, b, c):
     """Denne funktion modtager a, b og c-koefficienterne til en andengradsligning sat op på standardform som argumenter og returnerer de reelle løsninger."""
-    d = b**2 - (4*a * c)
+    
+    if zero_check(a, b, c):
+    
+    
+        d = b**2 - (4*a * c)
 
-    if d < 0:
-        print ("Ingen løsning")
-    elif d == 0:
-        x = -b/(2*a)
-        print(x)
-    elif d > 0:
-        x = (-b + d**0.5)/(2*a)
-        y = (-b - d**0.5)/(2*a)
-        if x <= y:
-            print(x,  y)
-        else:
-            print(y, x)
+        if d < 0:
+            window.solution_checker.setText("Inger røder")
+        elif d == 0:
+            x = -b/(2*a)
+            window.answer_1.setNum(x)
+            window.answer_2.setText("Ingen rod")
+        elif d > 0:
+            x = (-b + d**0.5)/(2*a)
+            y = (-b - d**0.5)/(2*a)
+            window.answer_1.setNum(x)
+            window.answer_2.setNum(y)
+            window.solution_checker.setText("Ingen problemer")
+        
+def mainwindow_setup(w):
+    """Hænger knappen sammen med matematikken"""
+    w.pushButton.clicked.connect(calculate)
+
+    
+def calculate():
+    """Kør matematikken"""
+    maths(window.number_A.value(), window.number_B.value(), window.number_C.value())
 
 loader = QUiLoader()
-
-
-
 app = QtWidgets.QApplication(sys.argv)
+
 window = loader.load("window.ui", None)
+mainwindow_setup(window)
 window.show()
 app.exec()
